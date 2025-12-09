b416bb28cb0479d180d0cdc6477337f6fa524e22
rene <linear23150@outlook.com>
2025-12-09 17:33:00 +0800

    7th change

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,4 +7,16 @@ import Layout from "./Layout.vue";
 export default {
   extends: DefaultTheme,
   Layout,
+  async enhanceApp() {
+    if (!import.meta.env.SSR) {
+      const { loadOml2d } = await import('oh-my-live2d');
+      loadOml2d({
+        models: [
+          {
+            path: 'https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json'
+          }
+        ]
+      });
+    }
+  }
 }
