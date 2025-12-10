affa50a6c366f61ae8bd0d7e3506c63ab6282347
rene <linear23150@outlook.com>
2025-12-10 11:11:48 +0800

    11th change

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import 'katex/dist/katex.min.css'
 import "@catppuccin/vitepress/theme/frappe/blue.css"
 import './var.css'
-
+import './code-block.css'
 import Layout from "./Layout.vue";
 
 export default {
